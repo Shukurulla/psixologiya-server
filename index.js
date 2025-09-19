ff864620e7395e7079d5c7230e89f60614c02d3e
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-09-19 16:18:51 +0500

    start

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
